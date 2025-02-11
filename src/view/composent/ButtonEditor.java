eddae5d18013fe886a543bd82091e4f5afca3336
Mathieu Bellegueulle <mathieu.bellegueulle@uha.fr>
2025-02-11 10:37:02 +0100

    Connection avec Interface pour vente de joueur

--- src/view/composent/ButtonEditor.java
+++ src/view/composent/ButtonEditor.java
@@ -13,20 +13,20 @@ import service.interfaces.JoueurService;
 // Custom editor to handle actions in JTable cells
 public class ButtonEditor extends AbstractCellEditor implements TableCellEditor {
     private EditButton editButton;
-    private DeleteButton deleteButton;
+    private SellButton sellButton;
     private JPanel panel;
 
     public ButtonEditor(JoueurService joueurService) {
         panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         editButton = new EditButton("Modifier", -1, joueurService);
-        deleteButton = new DeleteButton("Vendre", -1);
+        sellButton = new SellButton("Vendre", -1, joueurService);
         panel.add(editButton);
-        panel.add(deleteButton);
+        panel.add(sellButton);
 
         editButton.addActionListener(e -> {
             fireEditingStopped();  // Action handling logic
         });
-        deleteButton.addActionListener(e -> {
+        sellButton.addActionListener(e -> {
             fireEditingStopped();  // Action handling logic
         });
     }
@@ -34,7 +34,7 @@ public class ButtonEditor extends AbstractCellEditor implements TableCellEditor 
     public Component getTableCellEditorComponent(JTable table, Object value,
                                                  boolean isSelected, int row, int column) {
         editButton.setTargetId((int)value);  // Update button with the current row ID
-        deleteButton.setTargetId((int)value);
+        sellButton.setTargetId((int)value);
         return panel;
     }
 
