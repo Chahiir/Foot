e5ded3213afb0abda5e827d76aaf85c19b552c4c
Mathieu Bellegueulle <mathieu.bellegueulle@uha.fr>
2025-02-11 15:04:00 +0100

    Mise a jour Panneau de transfert

--- src/view/TransferPanel.java
+++ src/view/TransferPanel.java
@@ -10,8 +10,11 @@ import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
+import controller.Equipe;
+import controller.History;
 import controller.Joueur;
 import service.interfaces.EquipeService;
+import service.interfaces.HistoryService;
 import service.interfaces.JoueurService;
 import view.composent.ActionButton;
 import view.composent.TransferBuyButton;
@@ -22,44 +25,59 @@ public class TransferPanel extends JPanel{
     private DefaultTableModel model;
     private JoueurService joueurService;
     private EquipeService equipeService;
+    private HistoryService historyService;
     private int myTeamId;
 
     public TransferPanel(JoueurService joueurService, EquipeService equipeService) {
         super();
         this.joueurService = joueurService;  // Utiliser le service injecté
         this.equipeService = equipeService;  // Utiliser le service injecté
+        this.historyService = new HistoryService();
         setLayout(new BorderLayout());
         setBorder(new EmptyBorder(10, 10, 10, 10));
         setBackground(Color.WHITE);
-        
-        // Configuration des sous-panels
-        add(createPlayersForSalePanel(), BorderLayout.WEST);
-        add(createAvailablePlayersPanel(), BorderLayout.EAST);
+
         add(createTransferHistoryPanel(), BorderLayout.SOUTH);
+        // Configuration des sous-panels
+        JPanel centralPanel = new JPanel(new BorderLayout());
+        centralPanel.add(createPlayersForSalePanel(), BorderLayout.WEST);
+        centralPanel.add(new JPanel(), BorderLayout.CENTER);
+        centralPanel.add(createAvailablePlayersPanel(), BorderLayout.EAST);
+
+        add(centralPanel, BorderLayout.CENTER);
     }
 
     private JPanel createPlayersForSalePanel() {
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(new JLabel("Joueurs à Vendre"), BorderLayout.NORTH);
+        JPanel centralPanel = new JPanel();
         java.util.List<Joueur> playersToSell = joueurService.getPlayerToSell();
         if (playersToSell.isEmpty()) {
-            panel.add(new JLabel("Aucun joueur n'est actuellement à vendre."), BorderLayout.CENTER);
+            centralPanel.add(new JLabel("Aucun joueur n'est actuellement à vendre."), BorderLayout.CENTER);
         } else {
             for (Joueur joueur : playersToSell) {
                 if (joueur.getEquipe_id() == myTeamId)
-                    panel.add(createPlayerPanel(joueur, new TransferBuyButton(joueur.getId(), joueurService)));
+                    centralPanel.add(createPlayerPanel(joueur, new TransferBuyButton(joueur.getId(), joueurService)));
             }
         }
+        panel.add(centralPanel, BorderLayout.CENTER);
         return panel;
     }
 
     private JPanel createAvailablePlayersPanel() {
-        JPanel panel = new JPanel();
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(new JLabel("Joueurs à Acheter"), BorderLayout.NORTH);
         java.util.List<Joueur> playersToBuy = joueurService.getPlayerToSell();
-        for (Joueur joueur : playersToBuy) {
-            if (joueur.getEquipe_id() != myTeamId)
-                panel.add(createPlayerPanel(joueur, new TransferBuyButton(joueur.getId(), joueurService)));
+        JPanel centralPanel = new JPanel();
+        if (playersToBuy.isEmpty()) {
+            centralPanel.add(new JLabel("Aucun joueur n'est actuellement à vendre."), BorderLayout.CENTER);
+        } else {
+            for (Joueur joueur : playersToBuy) {
+                if (joueur.getEquipe_id() != myTeamId)
+                centralPanel.add(createPlayerPanel(joueur, new TransferBuyButton(joueur.getId(), joueurService)));
+            }
         }
+        panel.add(centralPanel, BorderLayout.CENTER);
         return panel;
     }
 
@@ -72,18 +90,25 @@ public class TransferPanel extends JPanel{
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(new JLabel("Historique des Transferts"), BorderLayout.NORTH);
 
-        String[] columnNames = {"Joueur", "Club Départ", "Club Arrivée", "Type de Transfert", "Montant", "Statut", "Date", "Actions"};
+        String[] columnNames = {"Joueur", "Club Départ", "Club Arrivée", "Type de Transfert", "Montant", "Date"};
         DefaultTableModel model = new DefaultTableModel(null, columnNames);
         JTable table = new JTable(model);
         JScrollPane scrollPane = new JScrollPane(table);
 
-        /* 
-        java.util.List<Transfer> history = joueurService.getTransferHistory();
-        for (Transfer transfer : history) {
-            model.addRow(new Object[]{transfer.getPlayerName(), transfer.getDepartingClub(), transfer.getArrivingClub(), 
-                                      transfer.getType(), transfer.getAmount(), transfer.getStatus(), transfer.getDate(), "Détails"});
+        
+        java.util.List<History> history = historyService.getHistory();
+        for (History transfer : history) {
+            if(transfer.getNewEquipe_id() == myTeamId || transfer.getOldEquipe_id()==myTeamId){
+                Joueur joueur = joueurService.getPlayerById(transfer.getJoueur_id());
+                Equipe departingClub = equipeService.getEquipeById(transfer.getOldEquipe_id());
+                Equipe arrivingClub = equipeService.getEquipeById(transfer.getNewEquipe_id());
+                String type = transfer.getNewEquipe_id() == myTeamId ? "Achat":"Vente";
+
+                model.addRow(new Object[]{joueur.getPrenom()+" "+joueur.getNom(), departingClub.getNom(), arrivingClub.getNom(), 
+                                        type, joueur.getPrix(), transfer.getDate()});
+            }
         }
-        */
+        
         panel.add(scrollPane, BorderLayout.CENTER);
         return panel;
     }
